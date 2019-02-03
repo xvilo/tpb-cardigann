b85efda1e126e64aa70161d05129e6a50fb04a81
Sem Schilder <sschilder@transip.nl>
2019-02-03 14:46:00 +0100

    Add page controller

---
+++ src/Controller/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * Matches / exactly
+     *
+     * @Route("/", name="search")
+     */
+    public function homepage()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+
+
+}
